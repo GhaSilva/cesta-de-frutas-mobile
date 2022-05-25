61b00238dcc78bf91fa51b9ca578667495426524
GhaSilva <ghabriel-silva@outlook.com>
2022-05-25 18:11:32 -0300

    Fim

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 
-import { StyleSheet, StatusBar, SafeAreaView, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta'
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <StatusBar/>
-      <Cesta/>  
-    </SafeAreaView>
-  );
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  })
+
+  if(!fonteCarregada){
+    return <AppLoading />
+  } else{
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <StatusBar/>
+        <Cesta {...mock}/>  
+      </SafeAreaView>
+    );
+  }
 }
